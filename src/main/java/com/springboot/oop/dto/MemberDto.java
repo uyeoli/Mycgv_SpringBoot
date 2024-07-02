8d3b5161fddf85c866b9eaa11ac589c9d3d83f70
uyjeong <uyjeong@interfaceit.co.kr>
2024-07-02 17:23:11 +0900

    2차 mybatis-oop 피드백

--- src/main/java/com/springboot/oop/dto/MemberDto.java
+++ src/main/java/com/springboot/oop/dto/MemberDto.java
@@ -7,12 +7,17 @@ import lombok.Setter;
 @Setter
 public class MemberDto {
 
-    private String id;
-    private String pass;
-    private String gender;
-    private String addr1;
-    private String addr2;
+    private String id; //수정, 삭제
+    private String pass; // 생성, 수정
+    private String gender; // 생성, 수정
+    private String addr1; // 생성, 수정
+    private String addr2; // 생성, 수정
 
+    /*
+    DTO의 분리는 역할에 따라 분리하면 된다. 위에 있는 역할은 생성, 수정, 삭제 3가지
+    DTO - MemberCreationDto, MemberUpdateDto, MemberDeleteDto 3가지로 분리 가능
+    컨트롤러에서 api마다 필요한 역할을 파라미터로 받아 역할에 맞게 사용 시 유지보수가 편리해진다.
+     */
     public String getAddress() {
         return getAddr1() + getAddr2();
     }
--- src/main/java/com/springboot/oop/service/impl/BoardServiceImpl.java
+++ src/main/java/com/springboot/oop/service/impl/BoardServiceImpl.java
@@ -3,6 +3,7 @@ package com.springboot.oop.service.impl;
 import com.springboot.oop.dto.BoardDto;
 import com.springboot.oop.entity.Board;
 import com.springboot.oop.mapper.BasicMapper;
+import com.springboot.oop.mapper.BoardMapper;
 import com.springboot.oop.repository.BoardRepository;
 import com.springboot.oop.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -15,17 +16,21 @@ import java.util.List;
 public class BoardServiceImpl implements BoardService {
 
     private final BoardRepository boardRepository;
-    private final BasicMapper basicMapper;
+    /*
+    다형성만 생각해서 인터페이스로 의존성을 할당할 필요가 없다.
+    제네릭을 사용해서 이미 타입을 확정해놨기 때문에 여기서 만약 인터페이스로 의존성 주입 시 명시적 형변환까지 해야하는 불필요한 코드가 필요하다.
+     */
+    private final BoardMapper boardMapper;
 
     @Override
     public List<BoardDto> findAll() {
         List<Board> list = boardRepository.findAll();
-        return basicMapper.toDtoList(list);
+        return boardMapper.toDtoList(list);
     }
 
     @Override
     public void modify(BoardDto boardDto) {
-        Board board = (Board) basicMapper.toEntity(boardDto);
+        Board board = boardMapper.toEntity(boardDto);
         boardRepository.modify(board);
     }
 
@@ -37,7 +42,7 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public void save(BoardDto boardDto) {
-        Board board = (Board) basicMapper.toEntity(boardDto);
+        Board board = boardMapper.toEntity(boardDto);
         boardRepository.save(board);
     }
 
